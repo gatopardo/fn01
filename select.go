bcb7d298b8547cd468183e70dec4412d1799c66b
Gato Pardo <gatopardo.codetel.net.do>
2018-09-11 21:01:49 -0400

    fb 2

--- select.go
+++ select.go
@@ -33,6 +33,7 @@ func main() {
   http.HandleFunc("/", DisplayRadioButtons)
   http.HandleFunc("/selected", UserSelected)
   http.HandleFunc("/home", HomePage)
+  http.HandleFunc("/test", Testfn)
 
 //  log.Fatal(http.ListenAndServe(":8080", nil))
 	fmt.Println("listening...")
@@ -105,12 +106,30 @@ func HomePage(w http.ResponseWriter, r *http.Request){
 
     t, err := template.ParseFiles("homepage.html") //parse the html file homepage.html
     if err != nil { // if there is an error
-  	  log.Print("template parsing error: ", err) // log it
-  	}
+	  log.Print("template parsing error: ", err) // log it
+	}
     err = t.Execute(w, HomePageVars) //execute the template and pass it the HomePageVars struct to fill in the gaps
     if err != nil { // if there is an error
-  	  log.Print("template executing error: ", err) //log it
-  	}
+	  log.Print("template executing error: ", err) //log it
+	}
+}
+
+func Testfn(w http.ResponseWriter, r *http.Request){
+
+    now := time.Now() // find the time right now
+    HomePageVars := HomeVariables{ //store the date and time in a struct
+      Date: now.Format("02-01-2006"),
+      Time: now.Format("15:04:05"),
+    }
+
+    t, err := template.ParseFiles("fn01.html") //parse the html file homepage.html
+    if err != nil { // if there is an error
+	  log.Print("template parsing error: ", err) // log it
+	}
+    err = t.Execute(w, HomePageVars) //execute the template and pass it the HomePageVars struct to fill in the gaps
+    if err != nil { // if there is an error
+	  log.Print("template executing error: ", err) //log it
+	}
 }
 
 
